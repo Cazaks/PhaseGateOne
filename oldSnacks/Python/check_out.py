2dabd49d7ebad7b8ea5f35c57a91da5dfe462108
Cazaks <calebezak@gmail.com>
2025-06-15 12:16:16 +0100

    Update on python

--- oldSnacks/Python/check_out.py
+++ oldSnacks/Python/check_out.py
@@ -1,32 +1,32 @@
-customer_name = input("What is the customer's name: ")
+customer_name = input("What is the customer's name:\n")
 	
-cashier_name = input("Enter cashier's name: ")
+cashier_name = input("Enter cashier's name:\n")
 
 total = 0; 
 vat = 0.075;
 
 while True:
-	user_purchased = input("What did the user buy?:")
+	user_purchased = input("What did the user buy?\n")
 
-	number_of_pieces = int(input("How many pieces:"))
+	number_of_pieces = int(input("How many pieces:\n"))
 
-	unit_price = float(input("How much per unit?:"))
+	unit_price = float(input("How much per unit?\n"))
 	
 	total = number_of_pieces * unit_price
 
-	customer_response = input ("Add more items? (yes/no): ")
+	customer_response = input ("Add more items? (yes/no):\n")
 	if customer_response == "no": 
 		break	
 	
-	amount_colected = ("How much did the customer gave you?: ")
+amount_colected = float(input("How much did the customer gave you?\n"))
 
-	discount_percentage = input("Enter discount percentage (7 for 7%): ")
-	discountpercentage = discount_percentage / 100
-	discount = total * discount_percentage
-	vat = total * vat
-	total_sum = total - discount + vat
-	total_amount = total + vat
-	balance_recieved = amount_colected - total_amount 
+discount_percentage = float(input("Enter discount percentage (7 for 7%):\n"))
+discount_percentage = discount_percentage / 100
+discount = total * discount_percentage
+vat = total * vat
+total_sum = total - discount + vat
+total_amount = total + vat
+balance_recieved = amount_colected - total_amount  
 
 print("SEMICOLON STORES")
 print("MAIN BRANCH")
@@ -34,7 +34,7 @@ print("LOCATION: 312, HERBERT MACAULAY WAY, SABO YABA, LAGOS.")
 print("TEL: 03293828343")
 print("DATE : 18-AUGUST-24  8:48:11pms")
 print(f"Cashier's name: {cashier_name}")
-print("Customer Name: customer_name}")
+print("Customer Name: {customer_name}")
 print("\n=========================================================\n")
 print(f"{'ITEM':>22}{'QTY':>10}{'PRICE':>10}{'TOTAL(NGN)':>15})")
 print("\n---------------------------------------------------------\n")
@@ -46,8 +46,8 @@ print(f"VAT (7.5%): {vat:36.2f} NGN")
 print(f"Total Payable: {total_sum:30.2f} NGN")
 	
 print("=========================================================")
-printf("Bill total: {total_amount:38.2f} NGN")
-print(f"Amount paid: {total_sum:30.2f} NGN")
+print(f"Total of amount paid:{total_amount:38.2f} NGN")
+print(f"Amount paid: {amount_colected:30.2f} NGN")
 print(f"Balance: {balance_recieved:36.2f} NGN")
 
 print("==========================================================")
